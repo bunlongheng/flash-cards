0e945040569abf534aa5df8cfc625ebbcfcba444
Bunlong Heng <bheng.code@gmail.com>
2023-05-12 12:21:02 -0400

    load same image now;

--- src/App.js
+++ src/App.js
@@ -4,31 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Thumbnail({ item, onItemClick }) {
 	const handleClick = () => {
-	  const utterance = new SpeechSynthesisUtterance(item.name);
-	  speechSynthesis.speak(utterance);
-	  onItemClick(item);
-	};
-  
-	const imageUrl = `https://source.unsplash.com/300x300/?shape,${item.image}?${item.name}`;
-  
+		const utterance = new SpeechSynthesisUtterance(item.name)
+		speechSynthesis.speak(utterance)
+		onItemClick(item)
+	}
+
+	const imageUrl = `https://source.unsplash.com/300x300/?shape,${item.image}?${item.name}`
+
 	return (
 		<div className="col-6 col-sm-4 col-md-3 col-lg-2 col-xl-1 mb-4">
-		<div className="thumbnail text-center" onClick={handleClick}>
-		  <img src={imageUrl} alt={item.name} className="img-thumbnail" />
-		  <div className="name mt-2">{item.name}</div>
+			<div className="thumbnail text-center" onClick={handleClick}>
+				<img src={imageUrl} alt={item.name} className="img-thumbnail" />
+				<div className="name mt-2">{item.name}</div>
+			</div>
 		</div>
-	  </div>
-	);
-  }
-  
+	)
+}
+
+function hexToRgb(hex) {
+	const r = parseInt(hex.substr(1, 2), 16)
+	const g = parseInt(hex.substr(3, 2), 16)
+	const b = parseInt(hex.substr(5, 2), 16)
+	return `${r}, ${g}, ${b}`
+}
+
 function Details({ item, handleClose }) {
 	const imageUrl = `https://source.unsplash.com/300x300/?shape,${item.image}?${item.name}`
 
 	const containerStyle = {
-		backgroundColor: `rgba(${item.color}, 0.8)`
+		backgroundColor: `rgba(${hexToRgb(item.color)}, 0.1)`
 	}
 
-	const excludedFields = ['id', 'name','color'];
+	const excludedFields = ['id', 'name']
 
 	return (
 		<div className="item-details-container border rounded" style={containerStyle} onClick={handleClose}>
@@ -36,16 +43,16 @@ function Details({ item, handleClose }) {
 				<img src={imageUrl} alt={item.name} className="img-thumbnail mb-3" style={{ width: '200px', height: '200px' }} />
 				<h2 className="mb-3">{item.name}</h2>
 				<div className="d-flex flex-column justify-content-between align-items-center">
-				{Object.keys(item).map((key) => {
-            if (!excludedFields.includes(key)) {
-              return (
-                <p key={key}>
-                  {key}: {item[key]}
-                </p>
-              );
-            }
-            return null;
-          })}
+					{Object.entries(item).map(([key, value]) => {
+						if (!excludedFields.includes(key)) {
+							return (
+								<p key={key}>
+									{key}: {value}
+								</p>
+							)
+						}
+						return null
+					})}
 				</div>
 			</div>
 		</div>
@@ -60,15 +67,15 @@ function App() {
 	}
 
 	const handleClose = (event) => {
-		event.stopPropagation();
-		setSelectedItem(null);
-	  };
+		event.stopPropagation()
+		setSelectedItem(null)
+	}
 
 	return (
 		<div className="app">
 			<h1 className="text-center mb-4" style={{ padding: '50px', margin: '50px' }}>
-        Shapes
-      </h1>
+				Shapes
+			</h1>
 			<div className="item-list container">
 				<div className="row">
 					{shapes.map((item) => (
