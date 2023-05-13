4d2233f221184ed7db574d50c5a5a4207268bd28
Bunlong Heng <bheng.code@gmail.com>
2023-05-13 06:27:51 -0400

    .env + landingPage;

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,75 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import './LandingPage.css'
+
+import { useDotenv } from 'react-dotenv'
 
 const LandingPage = () => {
-	const landingPageStyle = {
-		background: 'linear-gradient(135deg, #000000 0%, #333333 50%)',
-		backgroundSize: 'cover',
-		backgroundPosition: 'center',
-		height: '100vh',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center'
-	}
+	const { process } = useDotenv()
 
-	const linkContainerStyle = {
-		display: 'flex',
-		justifyContent: 'center',
-		marginTop: '2rem'
-	}
-
-	const linkStyle = {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		margin: '0 1rem',
-		textDecoration: 'none',
-		color: 'white',
-		textAlign: 'center'
-	}
-
-	const thumbnailStyle = {
-		width: '100px',
-		height: '100px',
-		border: '2px solid #ccc',
-		borderRadius: '5px',
-		overflow: 'hidden',
-		padding: '15px'
-	}
-
-	const imageStyle = {
-		width: '100%',
-		height: '100%',
-		objectFit: 'cover'
-	}
-
-	const [jsonFiles, setJsonFiles] = useState([])
-
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch('/api/jsons') // Replace with your API endpoint
-				const jsonFilesData = await response.json()
-				setJsonFiles(jsonFilesData)
-			} catch (error) {
-				console.error('Error fetching JSON files:', error)
-			}
-		}
-
-		fetchData()
-	}, [])
+	const typesString = process.env.TYPES || ''
+	const typesArray = typesString.split(',')
 
 	return (
-		<div style={landingPageStyle}>
+		<div className="landing-page">
 			<div className="container">
-				<div style={linkContainerStyle}>
-					{jsonFiles.map((data, index) => (
-						<Link key={index} to={`/${data.name}`} style={linkStyle}>
-							<div style={thumbnailStyle}>
-								<img src={`${process.env.PUBLIC_URL}/images/${data.name}.png`} alt={data.name} style={imageStyle} />
+				<div className="link-container">
+					{typesArray.map((data, index) => (
+						<Link key={index} to={`/${data}`} className="link-style">
+							<div className="thumbnail-style">
+								<img src={`${process.env.PUBLIC_URL}/images/${data}.png`} alt={data} className="image-style" />
 							</div>
-							<div>{data.name}</div>
+							<div>{data}</div>
 						</Link>
 					))}
 				</div>
