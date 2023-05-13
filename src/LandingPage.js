20047d59954435f7bf55c1f837e3c001dcdf945e
Bunlong Heng <bheng.code@gmail.com>
2023-05-13 19:00:57 -0400

    responsive;

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,15 +7,15 @@ const LandingPage = () => {
 	const typesArray = typesString.split(',')
 
 	return (
-		<div className="landing-page">
+		<div className="landing-page bg-dark text-light">
 			<div className="container">
-				<div className="link-container">
+				<div className="d-flex flex-wrap justify-content-center mt-4">
 					{typesArray.map((data, index) => (
-						<Link key={index} to={`/${data}`} className="link-style">
-							<div className="thumbnail-style">
+						<Link key={index} to={`/${data}`} className="link-style text-decoration-none text-light m-2">
+							<div className="thumbnail-style border border-secondary rounded p-3">
 								<img src={`${process.env.PUBLIC_URL}/images/${data}.png`} alt={data} className="image-style" />
 							</div>
-							<div>{data}</div>
+							<div className="mt-2">{data}</div>
 						</Link>
 					))}
 				</div>
