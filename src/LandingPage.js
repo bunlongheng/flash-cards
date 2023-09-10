dfbee214f7019f6ff6304a7440cfc9ff2bba8939
Bunlong Heng <bheng.code@gmail.com>
2023-09-10 14:41:35 -0400

    add speak to landing page clicked;

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './LandingPage.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
 const LandingPage = () => {
-	const typesString = process.env.REACT_APP_TYPES || ''
-	const typesArray = typesString.split(',')
+    const typesString = process.env.REACT_APP_TYPES || "";
+    const typesArray = typesString.split(",");
 
-	return (
-		<div className="landing-page bg-dark text-light">
-			<div className="container">
-				<div className="d-flex flex-wrap justify-content-center mt-4">
-					{typesArray.map((data, index) => (
-						<Link key={index} to={`/${data}`} className="link-style text-decoration-none text-light m-2">
-							<div className="thumbnail-style border border-secondary rounded p-3">
-								<img src={`${process.env.PUBLIC_URL}/images/${data}.png`} alt={data} className="image-style" />
-							</div>
-							<div className="mt-2">{data}</div>
-						</Link>
-					))}
-				</div>
-			</div>
-		</div>
-	)
-}
+    const speakAndRedirect = (text, redirectUrl) => {
+        const speech = new SpeechSynthesisUtterance(text);
+        speechSynthesis.speak(speech);
 
-export default LandingPage
+        setTimeout(() => {
+            window.location.href = redirectUrl;
+        }, 1000);
+    };
+
+    return (
+        <div className="landing-page bg-dark text-light">
+            <div className="container">
+                <div className="d-flex flex-wrap justify-content-center mt-4">
+                    {typesArray.map((data, index) => (
+                        <div key={index} className="link-style text-light m-2">
+                            <div className="thumbnail-style border border-secondary rounded p-3" onClick={() => speakAndRedirect(data, `/${data}`)}>
+                                <img src={`${process.env.PUBLIC_URL}/images/${data}.png`} alt={data} className="image-style" />
+                            </div>
+                            <div className="mt-2">{data}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
