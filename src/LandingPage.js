277dc20dbad8470b693488f4fc7819432996a6c7
Bunlong Heng <bheng.code@gmail.com>
2023-09-10 18:41:43 -0400

    put back main page for now;

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,30 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LandingPage.css";
 
 const LandingPage = () => {
     const typesString = process.env.REACT_APP_TYPES || "";
     const typesArray = typesString.split(",");
 
-    const speakAndRedirect = (text, redirectUrl) => {
-        const speech = new SpeechSynthesisUtterance(text);
-        speechSynthesis.speak(speech);
-
-        setTimeout(() => {
-            window.location.href = redirectUrl;
-        }, 1000);
-    };
-
     return (
         <div className="landing-page bg-dark text-light">
             <div className="container">
                 <div className="d-flex flex-wrap justify-content-center mt-4">
                     {typesArray.map((data, index) => (
-                        <div key={index} className="link-style text-light m-2">
-                            <div className="thumbnail-style border border-secondary rounded p-3" onClick={() => speakAndRedirect(data, `/${data}`)}>
+                        <Link key={index} to={`/${data}`} className="link-style text-decoration-none text-light m-2">
+                            <div className="thumbnail-style border border-secondary rounded p-3">
                                 <img src={`${process.env.PUBLIC_URL}/images/${data}.png`} alt={data} className="image-style" />
                             </div>
                             <div className="mt-2">{data}</div>
-                        </div>
+                        </Link>
                     ))}
                 </div>
             </div>
