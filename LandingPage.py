0965144f14f77c8c9248c7ab6e2963beddf2149a
Bunlong Heng <bheng.code@gmail.com>
2023-09-24 20:51:09 -0400

    Add more categories;

--- LandingPage.py
+++ LandingPage.py
@@ -1,9 +1,12 @@
-# Python Script - Update .env File with JSON File Names
 import os
+
 data_dir = "src/data"
 json_files = os.listdir(data_dir)
 names = [os.path.splitext(file)[0] for file in json_files]
 names_str = ",".join(names)
 env_file = ".env"
+
 with open(env_file, "w") as file:
     file.write(f"REACT_APP_TYPES={names_str}\n")
+
+print("Script has finished running and updated the .env file.")
