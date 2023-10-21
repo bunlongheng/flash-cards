b06c662cda667c742e48209054cdc646520b2392
Bunlong Heng <bheng.code@gmail.com>
2023-10-21 17:09:44 -0400

    add more transportations;

--- src/Categories.js
+++ src/Categories.js
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Categories.css";
 
 const Categories = () => {
-    const typesString = process.env.REACT_APP_TYPES || "";
-    const typesArray = typesString.split(",");
+    const appTypesString = process.env.REACT_APP_TYPES || "";
+    const appTypesArray = appTypesString.split(",");
+
+    const transportationTypesString = process.env.REACT_APP_TRANSPORTATION_TYPES || "";
+    const transportationTypesArray = transportationTypesString.split(",");
+
+    const [randomItem, setRandomItem] = useState("");
+
+    const getRandomItem = () => {
+        const randomIndex = Math.floor(Math.random() * transportationTypesArray.length);
+        return transportationTypesArray[randomIndex];
+    };
+
+    useEffect(() => {
+        console.log(transportationTypesArray);
+        const newItem = getRandomItem();
+        setRandomItem(newItem);
+    }, [getRandomItem, transportationTypesArray]);
 
     return (
         <div className="categories-page bg-dark text-light">
             <div className="sliding-text">
-                <span className="background-text">👋🏽 Welcome Back! Norden!</span>
+                <span className="background-text">👋🏽 Hi! Norden!</span>
                 &nbsp;----&nbsp;
-                <img src="/images/helicopter.png" alt="aircraft" height={30} />
+                <img src={`/images/${randomItem}.png`} alt={randomItem} height={30} />
             </div>
 
             <div className="container">
                 <div className="d-flex flex-wrap justify-content-center mt-4">
-                    {typesArray.map((data, index) => (
+                    {appTypesArray.map((data, index) => (
                         <Link key={index} to={`/${data}`} className="link-style text-decoration-none text-light m-2">
                             <div className="thumbnail-style border border-secondary rounded p-3">
                                 <img src={`${process.env.PUBLIC_URL}/images/types/${data}.png`} alt={data} className="image-style" />
--- updateEnv.py
+++ updateEnv.py
@@ -6,8 +6,18 @@ names = [os.path.splitext(file)[0] for file in json_files]
 names_str = ",".join(names)
 env_file = ".env"
 
+# Open the .env file in write mode to overwrite it
 with open(env_file, "w") as file:
     file.write(f"REACT_APP_TYPES={names_str}\n")
+    
+data_dir = "public/images/transportations"
+json_files = os.listdir(data_dir)
+names = [os.path.splitext(file)[0] for file in json_files]
+names_str = ",".join(names)
+
+# Append the REACT_APP_TYPES field to .env
+with open(env_file, "a") as file:
+    file.write(f"REACT_TRANSPORTATION_TYPES={names_str}\n")
 
 print("\n\n\n\n")
 print("\033[42;1;97mDone\033[0m: .env updated!")
