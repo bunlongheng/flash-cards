791e3321b6f7ae4f39ef088be05d4aa69305848b
Bunlong Heng <bheng.code@gmail.com>
2023-10-22 20:29:15 -0400

    no dark color;

--- src/Category.js
+++ src/Category.js
@@ -49,18 +49,32 @@ const Category = ({ type }) => {
     const getColor = (item, opacity = 0.6) => {
         if (item.color) {
             const hexColor = item.color;
-            const red = parseInt(hexColor.slice(1, 3), 16);
-            const green = parseInt(hexColor.slice(3, 5), 16);
-            const blue = parseInt(hexColor.slice(5, 7), 16);
-            return `rgba(${red}, ${green}, ${blue}, ${(opacity = 0.6)})`;
+            let red = parseInt(hexColor.slice(1, 3), 16);
+            let green = parseInt(hexColor.slice(3, 5), 16);
+            let blue = parseInt(hexColor.slice(5, 7), 16);
+
+            // Check if the color is too dark (close to black)
+            if (red + green + blue < 100) {
+                // If it's too dark, adjust the color
+                red = Math.floor(Math.random() * 256);
+                green = Math.floor(Math.random() * 256);
+                blue = Math.floor(Math.random() * 256);
+            }
+
+            return `rgba(${red}, ${green}, ${blue}, ${opacity})`;
         }
 
         let color = "rgba(";
-        for (let i = 0; i < 3; i++) {
-            const component = Math.floor(Math.random() * 256);
-            color += `${component},`;
-        }
-        color += `${opacity})`;
+        let red, green, blue;
+
+        // Generate random RGB values while avoiding black
+        do {
+            red = Math.floor(Math.random() * 256);
+            green = Math.floor(Math.random() * 256);
+            blue = Math.floor(Math.random() * 256);
+        } while (red + green + blue < 100); // Ensure it's not too dark
+
+        color += `${red}, ${green}, ${blue}, ${opacity})`;
         return color;
     };
 
