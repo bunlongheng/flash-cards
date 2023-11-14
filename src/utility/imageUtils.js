72435a02c6cbe359140c57ea4d8a8216fea9eff5
Bunlong Heng <bheng.code@gmail.com>
2023-11-14 14:53:30 -0500

    Good height/width for now;

--- src/utility/imageUtils.js
+++ src/utility/imageUtils.js
@@ -4,19 +4,25 @@ export const getImage = (item, type) => {
     if (localImageTypes.includes(type)) {
         const imageName = item.name.replace(/\s+/g, "-").toLowerCase();
 
-        //width 30%
         return `/images/${type}/${imageName}.png`;
     }
 
-    //width 100%
     return `https://source.unsplash.com/300x300/?${encodeURIComponent(item.name)}`;
 };
 
 // eslint-disable-next-line
-export const getImageWidth = (item, type) => {
+export const getImageWidth = type => {
     const localImageTypes = ["planets", "shapes", "polygons", "triangles", "construction-vehicles"];
     if (localImageTypes.includes(type)) {
         return `30%`;
     }
     return `100%`;
+};
+
+export const getImageHeight = type => {
+    const localImageTypes = ["planets", "shapes", "polygons", "triangles", "construction-vehicles"];
+    if (localImageTypes.includes(type)) {
+        return `auto`;
+    }
+    return `90%`;
 };
