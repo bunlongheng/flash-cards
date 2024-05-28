49edf402bd61a45c1eea66972a40649e4b7dd085
Bunlong Heng <bheng.code@gmail.com>
2024-05-28 15:26:46 -0400

    start.sh

---
+++ start.sh
@@ -0,0 +1,29 @@
+#1
+Give me 26 vegetables - A-Z in these format .json
+
+[
+	{
+		"name": "Carrot",
+	},
+	{
+		"name": "Broccoli",
+	},
+    ...
+]
+
+#2
+Create us-states.json
+
+#3
+/opt/homebrew/bin/python3 /Users/hengb01/Sites/flash-cards/updateEnv.py
+
+# Done: .env updated!
+# Add icon for new type(s): /Users/hengb01/Sites/flash-cards/public/images/types
+
+
+#4
+open /Users/hengb01/Sites/flash-cards/public/images/types
+
+
+#5
+npm start 
--- updateEnv.py
+++ updateEnv.py
@@ -10,7 +10,7 @@ env_file = ".env"
 # Open the .env file in write mode to overwrite it
 with open(env_file, "w") as file:
     file.write(f"REACT_APP_CATEGORIES={names_str}\n")
-    
+
 data_dir = "public/images/transportations"
 json_files = os.listdir(data_dir)
 names = [os.path.splitext(file)[0] for file in json_files]
@@ -24,3 +24,6 @@ print("\n\n\n\n")
 print("\033[42;1;97mDone\033[0m: .env updated!")
 print("\033[46;1;97mAdd icon for new type(s)\033[0m: /Users/hengb01/Sites/flash-cards/public/images/types")
 print("\n\n\n\n")
+
+
+os.system("open /Users/hengb01/Sites/flash-cards/public/images/types")
