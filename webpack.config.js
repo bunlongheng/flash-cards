b226e66ba690c8c1299bf767ad03bd305d722e63
Bunlong Heng <bheng.code@gmail.com>
2025-02-16 10:18:06 -0500

    webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
+    mode: "production", // or 'development' if debugging
     plugins: [
         new BundleAnalyzerPlugin({
-            analyzerMode: "server", // Opens in browser
+            analyzerMode: "static", // Change to 'server' if you want an interactive UI
+            openAnalyzer: true,
             generateStatsFile: true,
-            statsFilename: "stats.json",
+            statsFilename: "stats.json", // Generates the stats file
             logLevel: "info",
             defaultSizes: "parsed",
         }),
     ],
     stats: {
-        all: true,
+        all: true, // Ensures all modules are included
         modules: true,
         chunks: true,
         chunkModules: true,
